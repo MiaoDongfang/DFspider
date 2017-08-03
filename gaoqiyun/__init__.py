814807c1ef08287446facfc298eb45aa3ea3fa42
east <dongfang4021@gmail.com>
2017-08-03 15:50:23 +0800

    add readme.md file

--- gaoqiyun/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Standard Library
-
-
-__author__ = "dong fang"
--- test_spider/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-__author__ = "East"
-__created__ = "2017/3/18 23:32"
